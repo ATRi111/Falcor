631453225257149266029d76466e0a951c42883e
ATRi111 <1657125180@qq.com>
2025-12-03 11:30:21 +0400

    fix:Box Scene

--- scripts/Voxelization_CPU.py
+++ scripts/Voxelization_CPU.py
@@ -7,19 +7,22 @@ def render_graph_Pass():
     read_pass = createPass("ReadVoxelPass")
     marching_pass = createPass("RayMarchingPass")
     accumulate_pass = createPass("AccumulatePass", {"enabled": True, "precisionMode": "Single"})
+    ToneMapper = createPass("ToneMapper", {'autoExposure': False, 'exposureCompensation': 0.0})
 
     g.addPass(voxel_pass,"VoxelizationPass")
     g.addPass(read_pass,"ReadVoxelPass")
     g.addPass(marching_pass,"RayMarchingPass")
     g.addPass(accumulate_pass,"AccumulatePass")
+    g.addPass(ToneMapper, "ToneMapper")
 
     g.addEdge("VoxelizationPass.dummy","ReadVoxelPass.dummy")
     g.addEdge("ReadVoxelPass.vBuffer","RayMarchingPass.vBuffer")
     g.addEdge("ReadVoxelPass.gBuffer","RayMarchingPass.gBuffer")
 
     g.addEdge("RayMarchingPass.color","AccumulatePass.input")
-    g.markOutput("AccumulatePass.output")
-    
+    g.addEdge("AccumulatePass.output", "ToneMapper.src")
+    g.markOutput("ToneMapper.dst")
+
     return g
 
 Graph = render_graph_Pass()
